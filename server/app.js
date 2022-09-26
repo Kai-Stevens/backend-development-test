266fe6e36430b9102d68f111dea3ffa1c7330a2d
Kai Stevens <kai.stevens97@gmail.com>
2022-09-26 12:29:59 +0100

    configure server to handle POST requests

--- server/app.js
+++ server/app.js
@@ -1,8 +1,15 @@
 // This file starts the server off, describes the basic data, and creates the API
 const express = require("express");
-const beasts = require("./beasts")
+const cors = require("cors");
+
+const beasts = require("./beasts");
 // Make a basic server
 const app = express();
+
+//Allow requests from other origins
+app.use(cors());
+// Tell Express to always read the body of POST requests
+app.use(express.json());
 
 // Set up the server routes
 app.get("/", (request, response) => {
@@ -25,6 +32,15 @@ app.get("/beasts/random", (req, res) => {
 app.get("/beasts/:id", (req, res) => {
     const filtered = beasts.filter((beast) => beast.id == req.params.id);
     res.send(filtered[0]);
+});
+
+app.post("/beasts", (req, res) => {
+    // grab the beast data
+    const newBeast = req.body;
+    // add it to the list of beasts
+
+    // return a message saying it worked (if it hangs it's probably because you missed this part)
+    res.send(newBeast);
 });
 
 // Exports
